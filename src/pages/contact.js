4aeb5b472fb7ecd49bcccae1aa0806fc508413e9
mard-one <mardon.mashrabov@gmail.com>
2021-02-18 09:21:37 +0400

    Form UI

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,18 +1,153 @@
-import React from "react"
+import React, { useState, Fragment } from "react"
 import Layout from "../layout"
 import HeaderText from "../components/headerText"
 import Button from "../components/button"
 import Divider from "../components/divider"
-import { Grid, Typography } from "@material-ui/core"
+import { Grid, TextField, Typography } from "@material-ui/core"
 import Margin from "../components/margin"
+import { useStaticQuery } from "gatsby"
+import styled from "styled-components"
+
+const Input = styled.input`
+  width: 100%;
+  border: 0;
+  height: 28px;
+  display: block;
+  padding: 10px 0 10px;
+  box-sizing: content-box;
+  animation-duration: 10ms;
+  background-color: transparent;
+  color: white;
+  border-bottom: 1px solid #fff;
+  font-size: 18px;
+  font-family: "Graphik", Helvetica, Arial, sans-serif;
+  &:focus {
+    outline: none;
+  }
+`
+const LabelFloat = styled.label`
+  top: 0;
+  left: 0;
+  position: absolute;
+  transform: translate(0, 24px) scale(1);
+  transition: transform 200ms cubic-bezier(0, 0, 0.2, 1) 0ms;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 200%;
+  text-transform: uppercase;
+  color: #f2f3f1;
+  ${props =>
+    props.floating &&
+    `transform: translate(0, 1.5px) scale(0.75);
+    transform-origin: top left;`};
+`
+const InputField = ({
+  type,
+  id,
+  placeholder,
+  value,
+  handleOnChange,
+  as = "input",
+}) => {
+  const [floating, setFloating] = useState(false)
+  return (
+    <div
+      style={{
+        position: "relative",
+        display: "inline-flex",
+        flexDirection: "column",
+        width: "100%",
+      }}
+    >
+      <LabelFloat floating={floating || value} htmlFor={id}>
+        {placeholder}
+      </LabelFloat>
+      <div
+        style={{
+          marginTop: 16,
+        }}
+      >
+        <Input
+          as={as}
+          type={type}
+          id={id}
+          value={value}
+          onChange={handleOnChange}
+          onFocus={() => setFloating(true)}
+          onBlur={() => setFloating(false)}
+        />
+      </div>
+    </div>
+  )
+}
 
 const Contact = () => {
+  const [budget, setBudget] = useState([
+    { value: "value1", label: "$15.000 — $30.000", selected: false },
+    { value: "value2", label: "$30.000 — $60.000", selected: false },
+    { value: "value3", label: "$60.000 — $100.000", selected: false },
+    { value: "value4", label: "$100.000 — $150.000", selected: false },
+    { value: "value5", label: "$150.000 and up", selected: false },
+  ])
+  const selectedBudget =
+    budget.filter(obj => obj.selected) &&
+    budget.filter(obj => obj.selected)[0] &&
+    budget.filter(obj => obj.selected)[0].label
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [companyName, setCompanyName] = useState("")
+  const [detail, setDetail] = useState("")
+  const data = useStaticQuery(graphql`
+    query {
+      projectsJson {
+        projects {
+          type
+        }
+      }
+    }
+  `)
+  const projects = data.projectsJson.projects
+  const allProjectTypes = [...new Set(projects.map(project => project.type))]
+  const [selProjTypes, setSelProjTypes] = useState([])
+
+  const handleBudgetChange = event => {
+    event.persist()
+    setBudget(currentBudget => {
+      const mapReturn = currentBudget.map(budget => {
+        return budget.value === event.target.value
+          ? { ...budget, selected: true }
+          : { ...budget, selected: false }
+      })
+      return [...mapReturn]
+    })
+  }
+  const handleServiceCheck = event => {
+    const selProjType = event.target.value
+    if (event.target.checked) {
+      setSelProjTypes([...selProjTypes, selProjType])
+    } else {
+      const filtered = selProjTypes.filter(
+        type => type.toLowerCase() !== selProjType.toLowerCase()
+      )
+      setSelProjTypes([...filtered])
+    }
+  }
+  const handleSubmit = event => {
+    event.preventDefault()
+    console.log("selectedBudget", selectedBudget)
+    console.log("selProjTypes", selProjTypes)
+    console.log("name", name)
+    console.log("email", email)
+    console.log("companyName", companyName)
+    console.log("detail", detail)
+  }
+
   return (
     <Layout hideCTA>
       <HeaderText overline="CONTACT US">
         Interested in working with us? We’d love to hear from you.
       </HeaderText>
-      <Margin as="section" txs={500} bxs={110} tsm={150} bsm={100}>
+      <Margin as="section" txs={500} bxs={110} tsm={166} bsm={150}>
         <Grid container>
           <Grid item xs={12} sm={8}>
             <Typography variant="h3">
@@ -20,8 +155,6 @@ const Contact = () => {
               a process or experience? Please share your thoughts in the form
               below and we’ll be in touch.
             </Typography>
-            <Margin bxs={30} bsm={24} />
-            <Button>Or Schedule a 45 Min. Intro</Button>
           </Grid>
         </Grid>
       </Margin>
@@ -33,11 +166,32 @@ const Contact = () => {
             <Typography variant="h3">Services</Typography>
             <Margin bxs={20} bsm={20} />
             <div>
-              <Button style={{ marginRight: "16px", marginBottom: "16px" }}>Web Design</Button>
-              <Button style={{ marginRight: "16px", marginBottom: "16px" }}>App Design</Button>
-              <Button style={{ marginRight: "16px", marginBottom: "16px" }}>Branding</Button>
-              <Button style={{ marginRight: "16px", marginBottom: "16px" }}>Development</Button>
-              <Button style={{ marginRight: "16px", marginBottom: "16px" }}>Illustration</Button>
+              {allProjectTypes.map(type => {
+                return (
+                  <Fragment key={type}>
+                    <input
+                      type="checkbox"
+                      id={type}
+                      value={type}
+                      checked={selProjTypes.includes(type)}
+                      onChange={handleServiceCheck}
+                      style={{ display: "none" }}
+                    />
+                    <Button
+                      as="label"
+                      htmlFor={type}
+                      selected={selProjTypes.includes(type)}
+                      style={{
+                        marginRight: 16,
+                        marginBottom: 16,
+                        borderRadius: 21,
+                      }}
+                    >
+                      {type}
+                    </Button>
+                  </Fragment>
+                )
+              })}
             </div>
           </Grid>
         </Grid>
@@ -46,76 +200,93 @@ const Contact = () => {
           <Grid item xs={12} sm={8}>
             <Typography variant="h3">Budget in USD</Typography>
             <Margin bxs={20} bsm={20} />
-            <div>
-              <Button style={{ marginRight: "16px", marginBottom: "16px" }}>
-                $15.000 — $30.000{" "}
-              </Button>
-              <Button style={{ marginRight: "16px", marginBottom: "16px" }}>
-                $30.000 — $60.000{" "}
-              </Button>
-              <Button style={{ marginRight: "16px", marginBottom: "16px" }}>
-                $60.000 — $100.000{" "}
-              </Button>
-              <Button style={{ marginRight: "16px", marginBottom: "16px" }}>
-                $100.000 — $150.000{" "}
-              </Button>
-              <Button style={{ marginRight: "16px", marginBottom: "16px" }}>
-                $150.000 and up{" "}
-              </Button>
-            </div>
+            {budget.map(data => (
+              <Fragment key={data.value}>
+                <input
+                  type="radio"
+                  id={data.value}
+                  value={data.value}
+                  checked={data.selected}
+                  onChange={handleBudgetChange}
+                  name="budget"
+                  style={{ display: "none" }}
+                />
+                <Button
+                  as="label"
+                  selected={data.selected}
+                  htmlFor={data.value}
+                  style={{
+                    marginRight: 16,
+                    marginBottom: 16,
+                    borderRadius: 21,
+                  }}
+                >
+                  {data.label}
+                </Button>
+              </Fragment>
+            ))}
           </Grid>
         </Grid>
         <Margin bxs={70} bsm={70} />
         <Grid container>
           <Grid item xs={12} sm={6}>
-            <form>
-              <input
+            <form onSubmit={handleSubmit}>
+              <InputField
                 type="text"
+                id="name"
                 placeholder="Your Name"
-                style={{
-                  width: "100%",
-                  height: "48px",
-                  backgroundColor: "transparent",
-                  border: 0,
-                  borderBottom: "1px solid #FFFFFF",
-                }}
+                value={name}
+                handleOnChange={e => setName(e.target.value)}
               />
-              <input
+              <Margin bxs={30} bsm={40} />
+              <InputField
                 type="email"
+                id="email"
                 placeholder="Your Email"
-                style={{
-                  width: "100%",
-                  height: "48px",
-                  backgroundColor: "transparent",
-                  border: 0,
-                  borderBottom: "1px solid #FFFFFF",
-                }}
+                value={email}
+                handleOnChange={e => setEmail(e.target.value)}
               />
-              <input
+              <Margin bxs={30} bsm={40} />
+              <InputField
                 type="text"
+                id="company-name"
                 placeholder="Company Name"
-                style={{
-                  width: "100%",
-                  height: "48px",
-                  backgroundColor: "transparent",
-                  border: 0,
-                  borderBottom: "1px solid #FFFFFF",
-                }}
+                value={companyName}
+                handleOnChange={e => setCompanyName(e.target.value)}
               />
+              <Margin bxs={30} bsm={40} />
               <div>
-                <textarea
+                <InputField
+                  as="textarea"
+                  id="project-details"
+                  textarea
                   placeholder="Project Details"
-                  style={{
-                    width: "100%",
-                    height: "48px",
-                    backgroundColor: "transparent",
-                    border: 0,
-                    borderBottom: "1px solid #FFFFFF",
+                  value={detail}
+                  handleOnChange={e => {
+                    e.target.style.height = ""
+                    e.target.style.height =
+                      Math.min(e.target.scrollHeight, 200) + "px"
+                    setDetail(e.target.value)
                   }}
-                ></textarea>
-                <span>Need NDA first? E-mail us at hello@moonsight.com</span>
+                ></InputField>
+                <div
+                  style={{
+                    marginTop: 8,
+                    fontSize: "18px",
+                    lineHeight: "28px",
+                    color: "rgba(242, 243, 241, 0.54)",
+                  }}
+                >
+                  Need NDA first? E-mail us at{" "}
+                  <a
+                    href="mailto:hello@moonsight.com"
+                    style={{ color: "#F2F3F1" }}
+                  >
+                    hello@moonsight.com
+                  </a>
+                </div>
               </div>
-              <Margin bxs={30} bsm={56} />
+              <Margin bxs={30} bsm={49} />
               <Button
                 style={{
                   width: "273px",
